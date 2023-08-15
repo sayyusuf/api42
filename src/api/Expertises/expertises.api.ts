08fc0a8e0ff0feacc7eb826c868a9f17c11cfe00
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-15 21:13:12 +0300

    Update expertises.api.ts

--- src/api/Expertises/expertises.api.ts
+++ src/api/Expertises/expertises.api.ts
@@ -16,7 +16,7 @@ export class UsersApi {
      * @param 
      * @returns Return all the expertises
      */
-    public async get_expertises(params: EventsUsersParams): Promise<ExpertisesTypes>{
+    public async get_expertises(params: ExpertisesParams): Promise<ExpertisesTypes>{
         return await this.base.get("/v2/expertises/", params);
     }
 
@@ -25,7 +25,7 @@ export class UsersApi {
      * @param id
      * @returns Get an expertise
      */
-    public async get_expertises_with_id(id: string, params: EventsUsersParams): Promise<ExpertisesTypes>{
+    public async get_expertises_with_id(id: string, params: ExpertisesParams): Promise<ExpertisesTypes>{
         return await this.base.get("/v2/expertises/" + id, params);
     }
 }
