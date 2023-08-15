d8b407b76968ccb1266ff17c6de1ea40894c4810
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-15 20:56:37 +0300

    Create exams.api.ts

---
+++ src/api/Exams/exams.api.ts
@@ -0,0 +1,21 @@
+import { AnyTxtRecord } from "dns";
+import { ExamsParams } from "./exams.params.type";
+import {BaseApi42} from "../../base/baseApi42";
+
+export class UsersApi {
+    private base: BaseApi42;
+	
+    constructor (base: BaseApi42){
+		this.base = base;
+	}
+
+
+    /**
+     * api: `GET /v2/exams/:id  `
+     * @param id
+     * @returns Get an exam
+     */
+    public async get_events_users_with_id(id: string, params: EventsUsersParams): Promise<ExamsParams>{
+        return await this.base.get("/v2/exams/" + id, params);
+    }
+}
