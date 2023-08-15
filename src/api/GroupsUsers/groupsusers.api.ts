263196c6577524ee2bf62270293e451c2196ac0f
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-15 21:44:30 +0300

    Create groupsusers.api.ts

---
+++ src/api/GroupsUsers/groupsusers.api.ts
@@ -0,0 +1,50 @@
+import { AnyTxtRecord } from "dns";
+import { GroupsUsersParams } from "./groupsusers.params.types";
+import { GroupsUsersTypes } from "./groupsusers.types";
+import {BaseApi42} from "../../base/baseApi42";
+
+export class UsersApi {
+    private base: BaseApi42;
+	
+    constructor (base: BaseApi42){
+		this.base = base;
+	}
+
+
+    /**
+     * api: `GET /v2/groups_users   `
+     * @param 
+     * @returns Return all the groups users
+     */
+    public async get_groups_users(params: GroupsUsersParams): Promise<GroupsUsersTypes>{
+        return await this.base.get("/v2/groups_users/", params);
+    }
+
+    /**
+     * api: `GET /v2/groups_users/:id  `
+     * @param id
+     * @returns Get a groups user
+     */
+    public async get_groups_users_with_id(id: string, params: GroupsUsersParams): Promise<GroupsUsersTypes>{
+        return await this.base.get("/v2/groups_users/" + id, params);
+    }
+
+/**
+     * api: `GET /v2/users/:user_id/groups_users  `
+     * @param user_id
+     * @returns Return all the groups users of the given User
+     */
+    public async get_groups_users_with_user_id(user_id: string, params: GroupsUsersParams): Promise<GroupsUsersTypes>{
+        return await this.base.get("/v2/users/" + user_id + "/groups_users", params);
+    }
+
+/**
+     * api: `GET /v2/groups/:group_id/groups_users  `
+     * @param group_id
+     * @returns Return all the groups users of the given Group
+     */
+    public async get_groups_users_with_group_id(group_id: string, params: GroupsUsersParams): Promise<GroupsUsersTypes>{
+        return await this.base.get("/v2/groups/" + group_id + "/groups_users", params);
+    }
+   
+}
