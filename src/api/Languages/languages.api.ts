ea94ac8ea7b7dbfbac3cca3b7ec69fd973fef53e
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-15 21:51:05 +0300

    Create languages.api.ts

---
+++ src/api/Languages/languages.api.ts
@@ -0,0 +1,35 @@
+import { AnyTxtRecord } from "dns";
+import { LanguagesParams  } from "./languages.params.types";
+import { LanguageType  } from "./languages.types";
+import {BaseApi42} from "../../base/baseApi42";
+
+export class UsersApi {
+    private base: BaseApi42;
+	
+    constructor (base: BaseApi42){
+		this.base = base;
+	}
+
+
+    /**
+     * api: `GET /v2/languages   `
+     * @param 
+     * @returns Return all the languages
+     */
+    public async get_languages(params: LanguagesParams): Promise<LanguageType>{
+        return await this.base.get("/v2/languages/", params);
+    }
+
+
+    /**
+     * api: `GET /v2/languages/:id  `
+     * @param id
+     * @returns Get a language
+     */
+    public async get_languages_with_id(id: string, params: LanguagesParams): Promise<LanguageType>{
+        return await this.base.get("/v2/languages/" + id, params);
+    }
+
+
+   
+}
