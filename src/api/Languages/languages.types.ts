9da12494650fc748da345f48a1defd3ffe61ef41
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-15 21:46:54 +0300

    Create languages.types.ts

---
+++ src/api/Languages/languages.types.ts
@@ -0,0 +1,10 @@
+export type LanguagesParams = {
+  sort?: ParamStringLiterals;
+	filter?: ParamStringLiterals;
+	range?: ParamStringLiterals;
+	page?: any;
+	"page[number]"?: number;
+	"page[size]"?: number;
+}
+
+type ParamStringLiterals =  "id" | "name" | "identifier" | "created_at" | "updated_at";
