9273229da3cdd56973fff26b1c947d08d2d23a27
ysay <ysay@sayysf>
2023-08-17 19:16:32 +0300

    Broadcasts Ok.

---
+++ src/api/broadcasts/broadcasts.parasm.type.ts
@@ -0,0 +1,20 @@
+
+export type BroadcastsParam = {
+    sort?:                 "id" | "campus_id" | "tag_id" | "content" | "pinned_until" | "hidden_at" | "url" | "created_at" | "updated_at" | "content_html" | "position",
+    "filter[id]"?:          number[] | string[],
+    "filter[campus_id]"?:   number[] | string[],
+    "filter[tag_id]"?:      number[] | string[],
+    "filter[content]"?:     string[],
+    "filter[pinned_until]"?:string[],
+    "filter[hidden_at]"?:   string[],
+    "filter[url]"?:         string[],
+    "filter[created_at]"?:  string[],
+    "filter[updated_at]"?:  string[],
+    "filter[content_html]"?:string[],
+    "filter[position]"?:    string[],
+
+    page?:                  number,
+    "page[number]"?:        number,
+    "page[size]"?:          number,
+}
+
---
+++ src/api/broadcasts/broadcasts.type.ts
@@ -0,0 +1,10 @@
+export type Broadcast = {
+    id:             number,
+    campus_id:      number,
+    tag_id:         number,
+    content:        string,
+    pinned_until:   string,
+    hidden_at:      string,
+    url:            string,
+    created_at:     string,
+}
