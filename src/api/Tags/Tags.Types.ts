c4cb10e1cc4181cc5b4d67430fcf4cbb94a9f646
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-18 17:40:55 +0300

    Create Tags.Types.ts

---
+++ src/api/Tags/Tags.Types.ts
@@ -0,0 +1,15 @@
+export type TopLevel {
+  id:         number;
+  name:       string;
+  kind:       string;
+  users:      any[];
+  subnotions: Subnotion[];
+}
+
+export type Subnotion {
+  id:         number;
+  name:       string;
+  slug:       string;
+  created_at: string;
+  notepad:    null;
+}
