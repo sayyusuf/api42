887cb5872ef55be03651872df59774ec32968ff4
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-18 18:25:41 +0300

    Create teamsusers.types.ts

---
+++ src/api/TeamsUsers/teamsusers.types.ts
@@ -0,0 +1,48 @@
+export interface TeamsUsersTypes {
+  id:         number;
+  team_id:    number;
+  user_id:    number;
+  created_at: string;
+  validated:  boolean;
+  leader:     boolean;
+  occurrence: number;
+  team:       Team;
+  user:       TopLevelUser;
+}
+
+export interface Team {
+  id:                 number;
+  name:               string;
+  url:                string;
+  final_mark:         null;
+  project_id:         number;
+  created_at:         string;
+  updated_at:         string;
+  status:             string;
+  terminating_at:     null;
+  users:              UserElement[];
+  "locked?":          boolean;
+  "validated?":       null;
+  "closed?":          boolean;
+  repo_url:           null;
+  repo_uuid:          string;
+  locked_at:          string;
+  closed_at:          string;
+  project_session_id: number;
+}
+
+export interface UserElement {
+  id:               number;
+  login:            string;
+  url:              string;
+  leader:           boolean;
+  occurrence:       number;
+  validated:        boolean;
+  projects_user_id: number;
+}
+
+export interface TopLevelUser {
+  id:    number;
+  login: string;
+  url:   string;
+}
