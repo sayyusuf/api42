ed2cdde53cbcb3e23eda062ce8f69b66756e83a2
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-18 18:40:02 +0300

    Update feedbacks.api.ts

--- src/api/Feedbacks/feedbacks.api.ts
+++ src/api/Feedbacks/feedbacks.api.ts
@@ -2,7 +2,7 @@ import { AnyTxtRecord } from "dns";
 import { FeedbacksParams } from "./feedbacks.params.types";
 import {BaseApi42} from "../../base/baseApi42";
 
-export class UsersApi {
+export class FeedbacksApi {
     private base: BaseApi42;
 	
     constructor (base: BaseApi42){
