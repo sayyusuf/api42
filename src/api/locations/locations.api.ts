839fd5edbffed0f52b1a65b95fd7abe5af937214
Abdulselam Karahan <akarahan096@gmail.com>
2023-08-19 11:18:17 +0300

    complete locations api, TODO: tests

---
+++ src/api/locations/locations.api.ts
@@ -0,0 +1,61 @@
+import { BaseApi42 } from "../../base/baseApi42";
+
+export class LocationsApi {
+  private base: BaseApi42;
+
+  constructor(base: BaseApi42) {
+    this.base = base;
+  }
+
+  // GET /v2/locations/graph(/on/:field(/by/:interval))
+  /**
+   * Return grouped temporal data on locations
+   */
+  public async locations_graph(
+    field?: "created_at",
+    interval?: "month_of_year",
+    sort?: string,
+    filter?: string,
+    range?: string
+  ) {
+    return await this.base.get(`/v2/locations/graph`);
+  }
+
+  // GET /v2/locations
+  /**
+   * Return all the locations
+   */
+  public async locations() {
+    return await this.base.get(`/v2/locations`);
+  }
+
+  // GET /v2/users/:user_id/locations
+  /**
+   * Return all the locations of the given User
+   */
+  public async users_userId_locations(user_id: string) {
+    return await this.base.get(`/v2/users/${user_id}/locations`);
+  }
+
+  // GET /v2/campus/:campus_id/locations
+  /**
+   * Return all the locations of the given Campus
+   */
+  public async campus_campusId_locations(
+    campus_id: string,
+    sort?: string,
+    filter?: string,
+    range?: string,
+    page?: string
+  ) {
+    return await this.base.get(`/v2/campus/${campus_id}/locations`);
+  }
+
+  // GET /v2/locations/:id
+  /**
+   * Get a location
+   */
+  public async get_locations(id: string) {
+    return await this.base.get(`/v2/locations/${id}`);
+  }
+}
--- src/intrapi.ts
+++ src/intrapi.ts
@@ -1,10 +1,15 @@
 import { BaseApi42 } from "./base/baseApi42";
 import { UsersApi } from "./api/users/users.api"
 import { ApiData } from "./base/types";
+import { LanguageUsersApi } from "./api/languages_users/languages_users.api";
+import { LocationsApi } from "./api/locations/locations.api";
 
 export class IntraApi{
 	private base: BaseApi42
 	public users: UsersApi
+	public language_users: LanguageUsersApi
+	public locations: LocationsApi
+	
 	static async new(apiData: ApiData) : Promise<IntraApi>{
 		const base = await BaseApi42.new(apiData);
 		return new IntraApi(base)
@@ -12,6 +17,8 @@ export class IntraApi{
 	private constructor(baseApi: BaseApi42){
 		this.base = baseApi
 		this.users = new UsersApi(this.base)
+		this.language_users = new LanguageUsersApi(this.base)
+		this.locations = new LocationsApi(this.base)
 		// this.achievements = new AchievementsApi(this.base)
 	}
 }
