aed29281a4d69f350fa5040bb7d1bdc502d2031f
Abdulselam Karahan <akarahan096@gmail.com>
2023-08-19 11:10:58 +0300

    complete languages_users, TODO: tests

---
+++ src/api/languages_users/languages_users.api.ts
@@ -0,0 +1,61 @@
+import { BaseApi42 } from "../../base/baseApi42";
+
+export class LanguageUsersApi {
+  private base: BaseApi42;
+
+  constructor(base: BaseApi42) {
+    this.base = base;
+  }
+
+  // GET /v2/languages_users/graph(/on/:field(/by/:interval))
+  /**
+   * Return grouped temporal data on languages users
+   */
+  public async languages_users_graph(
+    field?: "created_at",
+    interval?: "month_of_year",
+    sort?: string,
+    filter?: string,
+    range?: string
+  ) {
+    return await this.base.get(`/v2/languages_users/graph`);
+  }
+
+  // GET /v2/users/:user_id/languages_users
+  /**
+   * Return all the languages users of the given User
+   */
+  public async users_languages_users(user_id: string) {
+    return await this.base.get(`/v2/users/${user_id}/languages_users`);
+  }
+
+  // GET /v2/languages_users
+  /**
+   * Return all the languages users
+   */
+  public async get_languages_users(
+    user_id?: string,
+    sort?: string,
+    filter?: string,
+    range?: string,
+    page?: string
+  ) {
+    return await this.base.get(`/v2/languages_users`);
+  }
+
+  // GET /v2/users/:user_id/languages_users/:id
+  /**
+   * Get a languages user of the given Id, associated with the given User
+   */
+  public async get_users_languages_users_id(user_id: string, id: string) {
+    return await this.base.get(`/v2/users/${user_id}/languages_users/${id}`);
+  }
+
+  // GET /v2/languages_users/:id
+  /**
+   * Get a languages user
+   */
+  public async get_languages_users_id(id: string) {
+    return await this.base.get(`/v2/languages_users/${id}`);
+  }
+}
---
+++ src/tests/languages_users.test.ts
@@ -0,0 +1,34 @@
+import { ApiData } from "../base/types";
+import { IntraApi } from "../intrapi";
+
+describe("Languages Users test suite", () => {
+  let intraApi;
+
+  beforeAll(async () => {
+    const api: {
+      client_id: string;
+      client_secret: string;
+      grant_type: string;
+      code?: string | undefined;
+      redirect_uri?: string | undefined;
+    } = require("./api.json");
+
+    const apiData: ApiData = {
+      client_id: api.client_id,
+      client_secret: api.client_secret,
+      grant_type: api.grant_type,
+      code: api.code,
+      redirect_uri: api.redirect_uri,
+    };
+
+    intraApi = await IntraApi.new(apiData);
+  });
+
+  it("should return language users grouped data", async () => {
+    const data = await intraApi.language_users
+      .languages_users_graph()
+      .catch((ex) => console.error(ex));
+
+    expect(0).toEqual(0);
+  });
+});
