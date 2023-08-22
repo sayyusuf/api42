3f1c5b9468afc53586dcf5d56f0d4249103a6723
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-22 22:11:12 +0300

    Update teams.params.types.ts

--- src/api/Teams/teams.params.types.ts
+++ src/api/Teams/teams.params.types.ts
@@ -1,15 +1,3 @@
 export type TeamsParams = {
-  
-	cursus_id?: string;
-  user_id?: string;
-  project_id?: string;
-  project_session_id?: string;
-  sort?: ParamStringLiterals;
-	filter?: ParamStringLiterals;
-	range?: ParamStringLiterals;
-	page?: any;
-	"page[number]"?: number;
-	"page[size]"?: number;
-}
 
-type ParamStringLiterals =  "id" | "project_id" | "name" | "created_at" | "updated_at" | "locked_at" | "closed_at" | "final_mark" | "repo_url" | "repo_uuid" | "deadline_at" | "terminating_at" | "project_session_id" | "status" | "cursus" | "active_cursus" | "campus" | "primary_campus" | "locked" | "closed" | "deadline" | "terminating" | "with_mark";
+}
