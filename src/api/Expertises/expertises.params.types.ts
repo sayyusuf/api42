6c05d8bf1689d037d62c8fc4e9a45015df82f9bf
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-22 21:46:02 +0300

    Update expertises.params.types.ts

--- src/api/Expertises/expertises.params.types.ts
+++ src/api/Expertises/expertises.params.types.ts
@@ -1,11 +1,3 @@
 export type ExpertisesParams = {
-  
-  sort?: ParamStringLiterals;
-	filter?: ParamStringLiterals;
-	range?: ParamStringLiterals;
-	page?: any;
-	"page[number]"?: number;
-	"page[size]"?: number;
-}
 
-type ParamStringLiterals =  "id" | "name" | "slug" | "created_at" | "updated_at" | "kind";
+}
