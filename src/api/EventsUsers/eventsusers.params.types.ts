0e41cda36d51d990b95d46af67f4bc89cdc7e141
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-22 21:43:00 +0300

    Update eventsusers.params.types.ts

--- src/api/EventsUsers/eventsusers.params.types.ts
+++ src/api/EventsUsers/eventsusers.params.types.ts
@@ -1,13 +1,4 @@
 export type EventsUsersParams = {
-  
-	user_id?: string;
-  event_id?: string;
-  sort?: ParamStringLiterals;
-	filter?: ParamStringLiterals;
-	range?: ParamStringLiterals;
-	page?: any;
-	"page[number]"?: number;
-	"page[size]"?: number;
+
 }
 
-type ParamStringLiterals =  "id" | "event_id" | "user_id" | "created_at" | "updated_at" | "campus_id";
