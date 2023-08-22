b52634876eb0cb4ea8932991cffcf68796dcf07f
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-22 22:05:59 +0300

    Update languages.params.types.ts

--- src/api/Languages/languages.params.types.ts
+++ src/api/Languages/languages.params.types.ts
@@ -1,10 +1,3 @@
 export type LanguagesParams = {
-  sort?: ParamStringLiterals;
-	filter?: ParamStringLiterals;
-	range?: ParamStringLiterals;
-	page?: any;
-	"page[number]"?: number;
-	"page[size]"?: number;
-}
 
-type ParamStringLiterals =  "id" | "name" | "identifier" | "created_at" | "updated_at";
+}
