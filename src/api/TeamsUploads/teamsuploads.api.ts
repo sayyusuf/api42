5db0722addbd97acf3420689bdd1f418d44dfc83
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-22 22:15:21 +0300

    Update teamsuploads.api.ts

--- src/api/TeamsUploads/teamsuploads.api.ts
+++ src/api/TeamsUploads/teamsuploads.api.ts
@@ -1,6 +1,6 @@
 import { AnyTxtRecord } from "dns";
 import { TeamsUploadsParams } from "./teamsuploads.params.types";
-import { TeamsUploadsTypes } from "./teamsuploads.types";
+import { TeamsUpload } from "./teamsuploads.types";
 import {BaseApi42} from "../../base/baseApi42";
 
 export class TeamsUploadsApi {
@@ -16,7 +16,7 @@ export class TeamsUploadsApi {
      * @param 
      * @returns Return all the teams uploads
      */
-    public async get_teams_uploads(params: TeamsUploadsParams): Promise<TeamsUploadsTypes>{
+    public async get_teamsUploads(params?: TeamsUploadsParams): Promise<TeamsUpload[]>{
         return await this.base.get("/v2/teams_uploads", params);
     }
 
@@ -25,7 +25,7 @@ export class TeamsUploadsApi {
      * @param id
      * @returns Get a teams upload
      */
-    public async get_teams_uploads_id(id:string, params: TeamsUploadsParams): Promise<TeamsUploadsTypes>{
+    public async get_teamsUploads_id(id:string, params?: TeamsUploadsParams): Promise<TeamsUpload>{
         return await this.base.get("/v2/teams_uploads/" + id, params);
     }
 
@@ -34,7 +34,7 @@ export class TeamsUploadsApi {
      * @param team_id
      * @returns Return all the teams uploads of the given Team
      */
-    public async get_teams_uploads_team_id(team_id:string, params: TeamsUploadsParams): Promise<TeamsUploadsTypes>{
+    public async get_teams_teamId_teamsUploads(team_id:string, params?: TeamsUploadsParams): Promise<TeamsUpload[]>{
         return await this.base.get("/v2/teams/" + team_id + "/teams_uploads", params);
     }
 }
