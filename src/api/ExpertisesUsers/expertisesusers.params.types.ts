2ce3523bc9b4d5840dc6332c095b38eb8284a03e
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-22 21:47:04 +0300

    Update expertisesusers.params.types.ts

--- src/api/ExpertisesUsers/expertisesusers.params.types.ts
+++ src/api/ExpertisesUsers/expertisesusers.params.types.ts
@@ -1,12 +1,4 @@
 export type ExpertisesUsersParams = {
-  expertise_id? : string;
-  user_id? : string;
-  sort?: ParamStringLiterals;
-	filter?: ParamStringLiterals;
-	range?: ParamStringLiterals;
-	page?: any;
-	"page[number]"?: number;
-	"page[size]"?: number;
+
 }
 
-type ParamStringLiterals =  "id" | "expertise_id" | "user_id" | "interested" | "value" | "created_at" | "updated_at" | "contact_me";
