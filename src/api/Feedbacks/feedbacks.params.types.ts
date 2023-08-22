2b15f9b392c50f836442ec7550f2a33feddc2852
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-22 21:47:36 +0300

    Update feedbacks.params.types.ts

--- src/api/Feedbacks/feedbacks.params.types.ts
+++ src/api/Feedbacks/feedbacks.params.types.ts
@@ -1,12 +1,4 @@
 export type FeedbacksParams = {
-  event_id? : string;
-  scale_team_id? : string;
-  sort?: ParamStringLiterals;
-	filter?: ParamStringLiterals;
-	range?: ParamStringLiterals;
-	page?: any;
-	"page[number]"?: number;
-	"page[size]"?: number;
+
 }
 
-type ParamStringLiterals =  "id" | "user_id" | "feedbackable_type" | "feedbackable_id" | "comment" | "rating" | "created_at" | "updated_at";
