ac2021928700cb1f21cfb3574a140428906ef5f8
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-22 22:16:00 +0300

    Update teamsusers.params.types.ts

--- src/api/TeamsUsers/teamsusers.params.types.ts
+++ src/api/TeamsUsers/teamsusers.params.types.ts
@@ -1,13 +1,3 @@
 export type TeamsUsersParams = {
-  
-	user_id?: string;
-  team_id?: string;
-  sort?: ParamStringLiterals;
-	filter?: ParamStringLiterals;
-	range?: ParamStringLiterals;
-	page?: any;
-	"page[number]"?: number;
-	"page[size]"?: number;
-}
 
-type ParamStringLiterals =  "id" | "team_id" | "user_id" | "leader" | "validated" | "created_at" | "updated_at" | "occurrence";
+}
