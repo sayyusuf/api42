19db43446d1a191b4d25fcf38b43eda46b8e5db1
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-22 21:55:21 +0300

    Update flags.params.types.ts

--- src/api/Flags/flags.params.types.ts
+++ src/api/Flags/flags.params.types.ts
@@ -1,10 +1,3 @@
 export type FlagsParams = {
-  sort?: ParamStringLiterals;
-	filter?: ParamStringLiterals;
-	range?: ParamStringLiterals;
-	page?: any;
-	"page[number]"?: number;
-	"page[size]"?: number;
-}
 
-type ParamStringLiterals =  "id" | "name" | "positive" | "icon" | "created_at" | "updated_at";
+}
