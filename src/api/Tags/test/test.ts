d1bf8464fd051af22e9c59d4be04703851a507d0
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-24 20:46:57 +0300

    Add files via upload

--- src/api/Tags/test/test.ts
+++ src/api/Tags/test/test.ts
@@ -19,7 +19,7 @@ const api : {client_id: string, client_secret: string, grant_type: string, code?
 
 		let base = await BaseApi42.new(apiData);
 		let ac = new TagsApi(base);
-		data = await ac.get_tag_user_id("12", {});
+		data = await ac.get_tags();
 		console.log(data)
 		writeFileSync("./res.json", JSON.stringify(data));
 	})() 
