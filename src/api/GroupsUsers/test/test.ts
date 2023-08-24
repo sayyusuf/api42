60836e4ad637b59ee6fe27d19a9dcde391e843ea
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-24 20:45:21 +0300

    Add files via upload

--- src/api/GroupsUsers/test/test.ts
+++ src/api/GroupsUsers/test/test.ts
@@ -19,7 +19,7 @@ const api : {client_id: string, client_secret: string, grant_type: string, code?
 
 		let base = await BaseApi42.new(apiData);
 		let ac = new GroupsUsersApi(base);
-		data = await ac.get_groups_users_user_id("12", {});
+		data = await ac.get_groupsUsers();
 		console.log(data)
 		writeFileSync("./res.json", JSON.stringify(data));
 	})() 
