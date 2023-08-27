620b616eb1110aede776d4f5747ce45f380b35a8
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-27 17:26:10 +0300

    Update groups.params.types.ts

--- src/api/Groups/groups.params.types.ts
+++ src/api/Groups/groups.params.types.ts
@@ -1,3 +1,8 @@
 export type GroupsParams = {
+    user_id?:            string | number,
+
+    page?:                          number,
+    "page[number]"?:                number,
+    "page[size]"?:                  number,
 
 }
