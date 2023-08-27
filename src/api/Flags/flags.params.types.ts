b817215065754d49743024c3b3ec5a48de228bff
Cagin Agirdemir <caginagirdemir@gmail.com>
2023-08-27 17:24:48 +0300

    Update flags.params.types.ts

--- src/api/Flags/flags.params.types.ts
+++ src/api/Flags/flags.params.types.ts
@@ -1,3 +1,23 @@
 export type FlagsParams = {
+    sort?:               "id" | "name" | "positive" | "icon" | "created_at" | "updated_at",
+  
+  
+    "filter[id]"?:string[] | number[],
+    "filter[name]"?:string[] | number[],
+    "filter[positive]"?:string[],
+    "filter[icon]"?:string[] | number[],
+    "filter[created_at]"?:string[],
+    "filter[updated_at]"?:string[],
+
+    "range[id]"?:string[] | number[],
+    "range[name]"?:string[] | number[],
+    "range[positive]"?:string[],
+    "range[icon]"?:string[] | number[],
+    "range[created_at]"?:string[],
+    "range[updated_at]"?:string[],
+
+    page?:                          number,
+    "page[number]"?:                number,
+    "page[size]"?:                  number,
 
 }
