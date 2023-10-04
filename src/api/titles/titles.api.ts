bf7d7eceb616a70c671e326aeec233df3df2ed5b
Abdulselam Karahan <akarahan096@gmail.com>
2023-10-04 11:25:17 +0300

    add params to titles

--- src/api/titles/titles.api.ts
+++ src/api/titles/titles.api.ts
@@ -1,4 +1,5 @@
 import { BaseApi42 } from "../../base/baseApi42";
+import { TitlesParams } from "./titles.params.type";
 import { Title } from "./titles.types";
 
 export class TitlesApi {
@@ -10,26 +11,33 @@ export class TitlesApi {
 
   // GET /v2/titles
   /**
-   * Return all the titles
+   *
+   * @param params
+   * @returns Return all the titles
    */
-  public async get_titles(): Promise<Title[]> {
-    return await this.base.get(`/v2/titles`);
+  public async get_titles(params?: TitlesParams): Promise<Title[]> {
+    return await this.base.get(`/v2/titles`, params);
   }
 
   // GET /v2/users/:user_id/titles
   /**
-   * Return all the titles of the given User
+   *
+   * @param user_id
+   * @param params
+   * @returns Return all the titles of the given User
    */
   public async get_users_userId_titles(
     user_id?: string,
-    page?: string
+    params?: TitlesParams
   ): Promise<Title[]> {
-    return await this.base.get(`/v2/users/${user_id}/titles`);
+    return await this.base.get(`/v2/users/${user_id}/titles`, params);
   }
 
   // GET /v2/titles/:id
   /**
-   * Get a title
+   *
+   * @param id
+   * @returns Get a title
    */
   public async get_titles_id(id?: string): Promise<Title> {
     return await this.base.get(`/v2/titles/${id}`);
