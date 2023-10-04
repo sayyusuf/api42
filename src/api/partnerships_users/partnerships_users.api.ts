31fda4ec968de373c6e2833486f354fa7a6284d3
Abdulselam Karahan <akarahan096@gmail.com>
2023-10-04 10:57:06 +0300

    add params to partnerships_users

--- src/api/partnerships_users/partnerships_users.api.ts
+++ src/api/partnerships_users/partnerships_users.api.ts
@@ -1,4 +1,5 @@
 import { BaseApi42 } from "../../base/baseApi42";
+import { PartnershipsUsersParams } from "./partnerships_users.params.type";
 import { PartnershipUsers } from "./partnerships_users.types";
 
 export class PartnershipsUsersApi {
@@ -10,36 +11,38 @@ export class PartnershipsUsersApi {
 
   // GET /v2/partnerships/:partnership_id/partnerships_users
   /**
-   * Return all the partnerships users of the given Partnership
+   *
+   * @param partnership_id
+   * @param params
+   * @returns Return all the partnerships users of the given Partnership
    */
   public async get_partnerships_partnershipId_partnerships_users(
-    partnership_id?: string,
-    sort?: string,
-    filter?: string,
-    range?: string,
-    page?: string
+    partnership_id: string,
+    params?: PartnershipsUsersParams
   ): Promise<PartnershipUsers[]> {
     return await this.base.get(
-      `/v2/partnerships/${partnership_id}/partnerships_users`
+      `/v2/partnerships/${partnership_id}/partnerships_users`,
+      params
     );
   }
 
   // GET /v2/partnerships_users
   /**
-   * Return all the partnerships users
+   *
+   * @param params
+   * @returns Return all the partnerships users
    */
   public async get_partnerships_users(
-    sort?: string,
-    filter?: string,
-    range?: string,
-    page?: string
+    params?: PartnershipsUsersParams
   ): Promise<PartnershipUsers[]> {
-    return await this.base.get(`/v2/partnerships_users`);
+    return await this.base.get(`/v2/partnerships_users`, params);
   }
 
   // GET /v2/partnerships_users/:id
   /**
-   * Get a partnerships user
+   *
+   * @param id
+   * @returns Get a partnerships user
    */
   public async get_partnerships_users_id(
     id: string
