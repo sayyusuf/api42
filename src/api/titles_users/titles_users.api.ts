cfbd2e04c91255bfec65c290d0279cc13eb2814c
Abdulselam Karahan <akarahan096@gmail.com>
2023-10-04 11:28:22 +0300

    add params to titles_users

--- src/api/titles_users/titles_users.api.ts
+++ src/api/titles_users/titles_users.api.ts
@@ -1,4 +1,5 @@
 import { BaseApi42 } from "../../base/baseApi42";
+import { TitlesUsersParams } from "./titles_users.params.type";
 import { TitleUser } from "./titles_users.types";
 
 export class TitlesUsersApi {
@@ -10,37 +11,49 @@ export class TitlesUsersApi {
 
   // GET /v2/titles/:title_id/titles_users
   /**
-   * Return all the titles users of the given Title
+   *
+   * @param title_id
+   * @param params
+   * @returns Return all the titles users of the given Title
    */
   public async get_titles_titleId_titles_users(
     title_id: string,
-    page?: string
+    params?: TitlesUsersParams
   ): Promise<TitleUser[]> {
-    return await this.base.get(`/v2/titles/${title_id}/titles_users`);
+    return await this.base.get(`/v2/titles/${title_id}/titles_users`, params);
   }
 
   // GET /v2/users/:user_id/titles_users
   /**
-   * Return all the titles users of the given User
+   *
+   * @param user_id
+   * @param params
+   * @returns Return all the titles users of the given User
    */
   public async get_users_userId_titles_users(
     user_id: string,
-    page?: string
+    params?: TitlesUsersParams
   ): Promise<TitleUser[]> {
-    return await this.base.get(`/v2/users/${user_id}/titles_users`);
+    return await this.base.get(`/v2/users/${user_id}/titles_users`, params);
   }
 
   // GET /v2/titles_users
   /**
-   * Return all the titles users
+   *
+   * @param params
+   * @returns Return all the titles users
    */
-  public async get_titles_users(page?: string): Promise<TitleUser[]> {
-    return await this.base.get(`/v2/titles_users`);
+  public async get_titles_users(
+    params?: TitlesUsersParams
+  ): Promise<TitleUser[]> {
+    return await this.base.get(`/v2/titles_users`, params);
   }
 
   // GET /v2/titles_users/:id
   /**
-   * Get a titles user
+   * 
+   * @param id 
+   * @returns Get a titles user
    */
   public async get_titles_users_id(id: string): Promise<TitleUser> {
     return await this.base.get(`/v2/titles_users/${id}`);
